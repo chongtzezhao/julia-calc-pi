d695d2173daaeb35f56765d58d9354b57c549e97
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-01-10 18:17:26 +0800

    Update mypi.jl

--- mypi.jl
+++ mypi.jl
@@ -1,5 +1,11 @@
 function mypi(n)
-    sum=0
-    for i=1 : 1000000 sum+=(1/(i^2)) end
-    return (sum*6)^0.5
+    ans=0
+    for i=1 : n
+		ans+=(1/(i^2))
+	end
+    return sqrt(ans*6)
+end
+
+for i=1 : 5
+    print("n=",string(10^i), ": ", mypi(10^i), "\n")
 end
