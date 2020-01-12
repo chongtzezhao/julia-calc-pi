979c70f2b8fd49e91d27fdd0bef4cc6daaf30403
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-01-12 22:54:41 +0800

    Update mypi.jl

--- mypi.jl
+++ mypi.jl
@@ -1,11 +1,11 @@
 function mypi(n)
-    ans=0
-    for i=1 : n
-		ans+=(1/(i^2))
-	end
-    return sqrt(ans*6)
-end
+    ans=0 # Initialize the answer as 0
+    for i=1 : n # Loop to iterate through n times
+	ans+=(1/(i^2)) # Answer is incremented to make it more precise
+    end # Break the loop
+    return sqrt(ans*6) # Conver Pi^2/6 to Pi
+end # End of function
 
 for i=1 : 5
-    print("n=",string(10^i), ": ", mypi(10^i), "\n")
+    print("n=",string(10^i), ": ", mypi(10^i), "\n") # prints successive powers of n, from n=1 to n=4, and their corresponding approxmimations of pi
 end
